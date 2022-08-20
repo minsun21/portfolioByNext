7679e0f3442b5f600032b7ce0daf67c993b97551
mspark <mspark0429@daum.net>
2022-08-20 23:22:11 +0900

    env & getStaticProps

--- pages/projects.js
+++ pages/projects.js
@@ -1,7 +1,9 @@
 import Head from 'next/head';
 import Layout from '../components/layout';
+import { DATABASE_ID, TOKEN } from '../config';
 
-export default function Projects() {
+export default function Projects({ projects }) {
+  console.log(projects);
   return (
     <Layout>
       <Head>
@@ -9,7 +11,41 @@ export default function Projects() {
         <meta name='description' content='MS Portfolio' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <h1>프로젝트</h1>
+      <h1>총 프로젝트: {projects.results.length}</h1>
+      {projects.results.map((project) => (
+        <h1 key={project.id}>{project.properties.Name.title[0].plain_text}</h1>
+      ))}
     </Layout>
   );
+}
+
+// 빌드 타임에 호출
+export async function getStaticProps() {
+  const options = {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Notion-Version': '2022-02-22',
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${TOKEN}`,
+    },
+    body: JSON.stringify({
+      sorts: [{ property: 'Name', direction: 'ascending' }],
+      page_size: 100,
+    }),
+  };
+
+  const res = await fetch(
+    `https://api.notion.com/v1/databases/${DATABASE_ID}/query`,
+    options
+  );
+  const projects = await res.json();
+
+  const projectsNames = projects.results.map(
+    (project) => project.properties.Name.title[0].plain_text
+  );
+
+  return {
+    props: { projects },
+  };
 }
